10df267c36817d8b7a8dce521a1aa931553d7366
buddybuild <help@buddybuild.com>
2017-08-17 10:56:00 -0700

    Update android sdk installer to not install emulators

--- search.sh
+++ search.sh
@@ -1,6 +1,8 @@
  /Users/rasharab/.android-sdk-installer/android-sdk-macosx/tools/bin/sdkmanager --update
  /Users/rasharab/.android-sdk-installer/android-sdk-macosx/tools/bin/sdkmanager --list --verbose |\
-    # Filter obsoleted packages
+    # This is a hack as 25.2.4- tools do not correctly list paths for sdkmanager
+    # grep -Riwo -e "path.*>" $ANDROID_STORE | awk -F'path="' -F'"' {'print $2'} | sort | uniq |\
+    # Filter packages
     egrep '^\S+'      |\
     sed -E '/(---)/d'  |\
     sed -E '/(Info)/d' |\
@@ -15,11 +17,9 @@
     sed -E '/(usb)/d' |\
     sed -E '/(x86)/d' |\
     sed -E '/(arm64)/d' |\
-    sed -E '/(add-ons;addon-google_apis-google-(15|16|17|18|19))/d' |\
-    # sed -E '/(system-images;android-(10|14|15|16|17|18|19|21|23);)/d' |\
-    # disable system images installation as there's a bad bug with installation
-    sed -E '/(system-images;android)/d' |\
-    # avoid installing emulator/tools due to forcing to 25.2.5
-    sed -E '/(^emulator)/d' |\
+    sed -E '/(add-ons;addon-google_apis-google-(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19))/d' |\
+    sed -E '/(lldb;2.(0|1|2))/d' |\
+    # emulators no longer really work in our environment
+    sed -E '/(system-images)/d' |\
     sed -E '/(^tools)/d' |\
-    sed '/\(done\)/d' > sdklist.txt
+    sed -E '/(done)/d'
